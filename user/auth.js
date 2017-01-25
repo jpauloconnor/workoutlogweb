938df27810288fedb8f0d0f2821dca58a5cb95ca
Paul O'Connor <jamespauloconnor@gmail.com>
2017-01-25 10:40:38 -0500

    Regulp

--- user/auth.js
+++ user/auth.js
@@ -84,7 +84,6 @@ $(function(){
          WorkoutLog.setAuthHeader(sessionToken);
          WorkoutLog.definition.fetchAll();
          WorkoutLog.log.fetchAll();
-         WorkoutLog.fetchAllFeeds();
          $(".disabled").removeClass("disabled");
          // change text of nav bar "login" to "logout"
          $("#loginout").text("Logout");
